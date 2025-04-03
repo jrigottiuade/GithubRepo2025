86d9773c408d6641971a3e8c9f9f299c0b482d66
titojp <titojp2002@hotmail.com>
2025-04-02 22:57:17 -0300

    update

--- src/components/code/DisplayCode.jsx
+++ src/components/code/DisplayCode.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import CopyIcon from "../../icons/CopyIcon";
+import CheckIcon from "../../icons/CheckIcon";
 
 const DisplayCode = ({ code, language = "javascript" }) => {
   const [copied, setCopied] = useState(false);
@@ -12,16 +14,18 @@ const DisplayCode = ({ code, language = "javascript" }) => {
 
   return (
     <div className="relative bg-gray-900 text-gray-100 p-4 rounded-lg shadow-md overflow-x-auto mb-3">
+      <p className="absolute top-2 left-5 text-sm bg-gray-700 px-2 rounded-xl">{language}</p>
       {/* Botón de Copiar */}
       <button
         onClick={copyToClipboard}
         className={
-            copied
-            ? "absolute top-2 right-2 bg-gray-700 text-white p-2 rounded-md transition"
-            : "absolute top-2 right-2 bg-gray-500 hover:bg-gray-300  text-white p-2 rounded-md transition"
+          copied
+            ? "absolute top-2 right-5 flex"
+            : "absolute top-2 right-5 flex cursor-pointer"
         }
       >
-        <span className="material-icons text-sm">content_copy</span>{" "}
+        {copied ? <CheckIcon width={15} /> : <CopyIcon width={15} />}
+        <span className="ps-1 text-xs">{copied ? "Copiado" : "Copiar"}</span>
       </button>
 
       {/* Muestra el código con formato */}
