5ce2792613460b89433e9b3b1ffc37824f96096c
Juan Pablo Rigotti <juan.rigotti@bue.edu.ar>
2025-05-22 17:40:16 -0300

    update

--- src/pages/js/subpages/ejercicio1/componentes/Ejercicio1Step3.jsx
+++ src/pages/js/subpages/ejercicio1/componentes/Ejercicio1Step3.jsx
@@ -11,10 +11,11 @@ const Ejercicio1Step3 = () => {
         vamos a generar funciones de acuerdo a las siguientes consignas:
       </p>
 
+
       <p className="title-h2-underline pt-5">Consgina 1</p>
       <ul className="list-decimal list-inside space-y-2 text-gray-800">
-        <li>Crear una función llamada mostrarDestinos()</li>
-        <li>Esta función no recibe ningún argumento</li>
+        <li>Crear una función llamada iterarArray(paquetes)</li>
+        <li>Esta función recibe como parámetro el array paquetes a iterar</li>
         <li>
           Dentro de la función usar un ciclo a tu preferencia, de manera que
           itere el array y muestre los elementos en consola
@@ -24,11 +25,15 @@ const Ejercicio1Step3 = () => {
 
       <p className="title-h2-underline pt-5">Consgina 2</p>
       <ul className="list-decimal list-inside space-y-2 text-gray-800">
-        <li>Crear una función llamada iterarArray(paquetes)</li>
-        <li>Esta función recibe como argumento el array paquetes a iterar</li>
         <li>
-          Dentro de la función usar un ciclo a tu preferencia, de manera que
-          itere el array y muestre los elementos en consola
+          Crear una función llamada insertarDestino(paquetes, destino)
+        </li>
+        <li>Esta función recibe 2 argumentos</li>
+        <li>Primer parámetro: el array que se quiere modificar</li>
+        <li>Segundo parámetro: el destino a agregar</li>
+        <li>
+          La función deberá retornar true si el destino fue insertado
+          exitosamente
         </li>
         <li>Invocar a la función y validar que cumpla la consigna</li>
       </ul>
@@ -36,24 +41,11 @@ const Ejercicio1Step3 = () => {
       <p className="title-h2-underline pt-5">Consgina 3</p>
       <ul className="list-decimal list-inside space-y-2 text-gray-800">
         <li>
-          Crear una función llamada insertarDestino(paquetes, destino, posición)
+          Crear una función llamada buscarDestino(paquetes, destino)
         </li>
-        <li>Esta función recibe 3 argumentos</li>
-        <li>Primer argumento: el array que se quiere modificar</li>
-        <li>Segundo argumento: el destino a agregar</li>
-        <li>
-          Tercer argumento: la posición donde se quiere agregar el destino a
-          través del índice
-        </li>
-        <li>
-          Si la función no recibiera el tercer argumento, asumir por defecto que
-          es al final
-        </li>
-        <li>
-          La función deberá retornar true si el destino fue insertado
-          exitosamente
-        </li>
-        <li>Invocar a la función y validar que cumpla la consigna</li>
+        <li>Primer parámetro: el array que se quiere observar</li>
+        <li>Segundo parámetro: el destino a buscar</li>
+        <li>La función debe mostrar en consola si se encontró o no </li>
       </ul>
     </div>
   );
